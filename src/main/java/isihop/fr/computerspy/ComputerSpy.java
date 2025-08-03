36e13b82a4333e33798b6ad04a1ab8af8c71fc7c
tondeur-h <tondeur-h@tondeur-h-OptiPlex-3040>
2025-08-03 16:14:07 +0200

    init 20250803

--- src/main/java/isihop/fr/computerspy/ComputerSpy.java
+++ src/main/java/isihop/fr/computerspy/ComputerSpy.java
@@ -20,10 +20,10 @@ import oshi.util.FormatUtil;
 
 public class ComputerSpy {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) 
+    {
         SystemInfo systemInfo = new SystemInfo();
         OperatingSystem os = systemInfo.getOperatingSystem();
-
         HardwareAbstractionLayer hardware = systemInfo.getHardware();
         CentralProcessor processor = hardware.getProcessor();
         Sensors sensors = hardware.getSensors();
@@ -37,7 +37,38 @@ public class ComputerSpy {
         List<NetworkIF> cnets = hardware.getNetworkIFs(true);
 
         header();
+        infosystem(os);
+        infoProcessor(processor);
+        infoSensors(sensors);
+        infoMemory(hardware);
+        infoDisk(hwds);
+        computer(computer);
+        graphicsCards(gcards);
+        monitors(gdisps);
+        soundCards(scards);
+        usbDevices(usbs); 
+        powers(psources);
+        networks(cnets);
+    }
 
+    /**
+     * ***************************
+     * Entete
+     ****************************
+     */
+    private static void header() {
+        System.out.println("\n================================");
+        System.out.println("     Computer spy v1.0");
+        System.out.println("isihop.fr : Tondeur Hervé (2025)");
+        System.out.println("================================");
+    }
+    
+    /******************************
+     * 
+     * @param os 
+     ******************************/
+    private static void infosystem(OperatingSystem os)
+    {
         System.out.println("\n=== Informations sur le système ===");
         System.out.println();
         System.out.println("Système d'exploitation : " + os);
@@ -45,40 +76,75 @@ public class ComputerSpy {
         System.out.println("Uptime :"+FormatUtil.formatElapsedSecs(os.getSystemUptime()));
         System.out.println("Nombre de Threads :"+os.getThreadCount());
 
-        System.out.println("\n=== Informations sur le processeur ===");
-        System.out.println("Nom : " + processor.getProcessorIdentifier().getName());
-        System.out.println("Fabricant : " + processor.getProcessorIdentifier().getVendor());
-        System.out.println("Architecture : " + processor.getProcessorIdentifier().getMicroarchitecture());
-        System.out.println("Fréquence maximale : " + FormatUtil.formatHertz(processor.getMaxFreq()));
-        System.out.println("Nombre de cœurs physiques : " + processor.getPhysicalProcessorCount());
-        System.out.println("Nombre de cœurs logiques : " + processor.getLogicalProcessorCount());
-        System.out.println("Identifiant : " + processor.getProcessorIdentifier().getIdentifier());
-
-        System.out.println("\n=== Charge CPU ===");
-        for (int i = 0; i < processor.getCurrentFreq().length; i++) {
-            System.out.println("Fréquence actuelle CPU [" + i + "] : " + FormatUtil.formatHertz(processor.getCurrentFreq()[i]));
+    }
+    
+    /******************************
+     * 
+     * @param processor 
+     ******************************/
+    private static void infoProcessor(CentralProcessor processor)
+    {
+        try {
+            System.out.println("\n=== Informations sur le processeur ===");
+            System.out.println("Nom : " + processor.getProcessorIdentifier().getName());
+            System.out.println("Fabricant : " + processor.getProcessorIdentifier().getVendor());
+            System.out.println("Architecture : " + processor.getProcessorIdentifier().getMicroarchitecture());
+            System.out.println("Fréquence maximale : " + FormatUtil.formatHertz(processor.getMaxFreq()));
+            System.out.println("Nombre de cœurs physiques : " + processor.getPhysicalProcessorCount());
+            System.out.println("Nombre de cœurs logiques : " + processor.getLogicalProcessorCount());
+            System.out.println("Identifiant : " + processor.getProcessorIdentifier().getIdentifier());
+            
+            System.out.println("\n=== Charge CPU ===");
+            for (int i = 0; i < processor.getCurrentFreq().length; i++) {
+                System.out.println("Fréquence actuelle CPU [" + i + "] : " + FormatUtil.formatHertz(processor.getCurrentFreq()[i]));
+            }
+            // Capture des ticks initiaux
+            long[][] oldTicks = processor.getProcessorCpuLoadTicks();
+            
+            // Attente d'une seconde
+            Thread.sleep(1000);
+            double[] load = processor.getProcessorCpuLoadBetweenTicks(oldTicks);
+            for (int i = 0; i < load.length; i++) {
+                System.out.printf("Cœur %d : %.1f%%%n", i, load[i] * 100);
+            }
+        } catch (InterruptedException ex) {
+            System.getLogger(ComputerSpy.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         }
-        // Capture des ticks initiaux
-        long[][] oldTicks = processor.getProcessorCpuLoadTicks();
-
-        // Attente d'une seconde
-        Thread.sleep(1000);
-        double[] load = processor.getProcessorCpuLoadBetweenTicks(oldTicks);
-        for (int i = 0; i < load.length; i++) {
-            System.out.printf("Cœur %d : %.1f%%%n", i, load[i] * 100);
-        }
-
+    }
+    
+    /**************************
+     * 
+     * @param sensors 
+     **************************/
+    private static void infoSensors(Sensors sensors)
+    {
         System.out.println("\n=== Température et capteurs ===");
         System.out.println("Température CPU : " + sensors.getCpuTemperature() + " °C");
         System.out.println("Tension CPU : " + sensors.getCpuVoltage() + " V");
         System.out.println("Ventilateurs : " + sensors.getFanSpeeds().length + " détectés");
 
+    }
+    
+    /***************************
+     * 
+     * @param hardware 
+     ***************************/
+    private static void infoMemory(HardwareAbstractionLayer hardware)
+    {
         System.out.println("\n=== Mémoire ===");
         System.out.println("Mémoire totale : " + FormatUtil.formatBytes(hardware.getMemory().getTotal()));
         System.out.println("Mémoire disponible : " + FormatUtil.formatBytes(hardware.getMemory().getAvailable()));
-
-        System.out.println("\n=== Disques ===");
-        for (HWDiskStore hwd : hwds) {
+    }
+    
+    /****************************
+     * 
+     * @param hwds 
+     ****************************/
+    private static void infoDisk(List<HWDiskStore> hwds)
+    {
+            System.out.println("\n=== Disques ===");
+        for (HWDiskStore hwd : hwds) 
+        {
             System.out.println("Disque [" + hwd.getName() + "] : " + hwd.getModel());
             System.out.println("\tNuméro de série :" + " " + hwd.getSerial());
             System.out.println("\tTaille : " + FormatUtil.formatValue(hwd.getSize(), "o"));
@@ -95,7 +161,15 @@ public class ComputerSpy {
             }
             System.out.println("");
         }
-
+    }
+    
+    
+    /***************************
+     * 
+     * @param computer 
+     ***************************/
+    private static void computer(ComputerSystem computer)
+    {
         System.out.println("\n=== Chassis ===");
         System.out.println("Chassis : " + computer.getBaseboard());
         System.out.println("Fabricant : " + computer.getManufacturer());
@@ -104,8 +178,15 @@ public class ComputerSpy {
         System.out.println("Firmware : " + computer.getFirmware());
         System.out.println("HardWare UUID : " + computer.getHardwareUUID());
 
-        
-        System.out.println("\n=== Cartes graphiques ===");
+    }
+    
+    /**************************
+     * 
+     * @param gcards 
+     **************************/
+    private static void graphicsCards(List<GraphicsCard> gcards)
+    {
+                System.out.println("\n=== Cartes graphiques ===");
         int numcard = 0;
         if (!gcards.isEmpty())
         {
@@ -124,9 +205,15 @@ public class ComputerSpy {
             System.out.println("Non reconnu !");
         }
         System.out.println("");
-
-        
-        
+    }
+    
+    
+    /*************************
+     * 
+     * @param gdisps 
+     *************************/
+    private static void monitors(List<Display> gdisps)
+    {
         System.out.println("\n=== Moniteurs ===");
         int numdisp = 0;        
         if (!gdisps.isEmpty()) {
@@ -140,6 +227,15 @@ public class ComputerSpy {
         }
         System.out.println("");
 
+    }
+
+    
+    /**************************
+     * 
+     * @param scards 
+     **************************/
+    private static void soundCards(List<SoundCard> scards)
+    {
         System.out.println("\n=== Cartes son ===");
         int numsound = 0;
         if (!scards.isEmpty()) {
@@ -155,6 +251,14 @@ public class ComputerSpy {
         }
         System.out.println("");
 
+    }
+
+    /***************************
+     * 
+     * @param usbs 
+     **************************/
+    private static void usbDevices(List<UsbDevice> usbs) 
+    {
         System.out.println("\n=== USB devices ===");
         int numusb = 0;
         for (UsbDevice usb : usbs) {
@@ -169,7 +273,14 @@ public class ComputerSpy {
             numusb++;
         }
         System.out.println("");
-
+    }
+    
+    /****************************
+     * 
+     * @param psources 
+     ****************************/
+    private static void powers(List<PowerSource> psources) 
+    {
         System.out.println("\n=== Alimentations ===");
         int numalim = 0;
         if (!psources.isEmpty()) {
@@ -187,7 +298,14 @@ public class ComputerSpy {
             System.out.println("Non reconnu !");
         }
         System.out.println("");
+    }
 
+    /*************************
+     * 
+     * @param cnets 
+     *************************/
+    private static void networks(List<NetworkIF> cnets) 
+    {
         System.out.println("\n=== Cartes Réseaux ===");
         int numnet = 0;
         for (NetworkIF cnet : cnets) {
@@ -205,20 +323,6 @@ public class ComputerSpy {
             numnet++;
         }
         System.out.println("");
-
     }
-
-    /**
-     * ***************************
-     * Entete
-     ****************************
-     */
-    private static void header() {
-        System.out.println("\n================================");
-        System.out.println("     Computer spy v1.0");
-        System.out.println("isihop.fr : Tondeur Hervé (2025)");
-        System.out.println("================================");
-    }
-    
     
 }
